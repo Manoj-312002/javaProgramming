8c97e3a3e05f1e49d2d94ee3c92d54549edbea65
Manoj-312002 <manoj.selvam312@gmail.com>
2021-11-17 17:26:37 +0530

    pinh3

---
+++ simple/computerNetworks/lab12/ping2.java
@@ -0,0 +1,21 @@
+package computerNetworks.lab12;
+import java.util.Scanner;
+
+public class ping2 {
+    public static void main(String[] args) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter host name : ");
+        String ip = sc.nextLine();
+        System.out.print("Enter number of packets : ");
+        int np = sc.nextInt();
+
+        Process p = Runtime.getRuntime().exec("ping -n " + np + " " + ip );
+        var st = p.getInputStream();
+        StringBuilder route = new StringBuilder();
+        int r;
+        while( (r = st.read() )!= -1 ) route.append( (char)r );
+        
+        System.out.println(route);
+        sc.close();
+    }
+}
